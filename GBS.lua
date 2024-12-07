0e6d683a913dcb2b62b819e24d6a5d3d9d67a2de
j1w0o <koreaminestudio@gmail.com>
2024-12-07 21:46:09 +0900

    GBS.lua

--- GBS.lua
+++ GBS.lua
@@ -1,7 +1,7 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-   Name = "GBS HUB (개병신 hub)",
+   Name = "GBS HUB",
    LoadingTitle = "정식 릴리즈!",
    LoadingSubtitle = "by j1w0o.",
    Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
@@ -61,13 +61,6 @@ local Button = MainTab:CreateButton({
 })
 
 local Button = MainTab:CreateButton({
-   Name = "Fly",
-   Callback = function()
-       loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\40\39\104\116\116\112\115\58\47\47\103\105\115\116\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\109\101\111\122\111\110\101\89\84\47\98\102\48\51\55\100\102\102\57\102\48\97\55\48\48\49\55\51\48\52\100\100\100\54\55\102\100\99\100\51\55\48\47\114\97\119\47\101\49\52\101\55\52\102\52\50\53\98\48\54\48\100\102\53\50\51\51\52\51\99\102\51\48\98\55\56\55\48\55\52\101\98\51\99\53\100\50\47\97\114\99\101\117\115\37\50\53\50\48\120\37\50\53\50\48\102\108\121\37\50\53\50\48\50\37\50\53\50\48\111\98\102\108\117\99\97\116\111\114\39\41\44\116\114\117\101\41\41\40\41\10\10")()
-   end,
-})
-
-local Button = MainTab:CreateButton({
    Name = "Blox Fruits",
    Callback = function()
        loadstring(game.HttpGet(game,'https://raw.githubusercontent.com/Yumiara/Python/refs/heads/main/BloxFruit-XYZ.lua'))()
@@ -85,25 +78,43 @@ local Button = MainTab:CreateButton({
    end,
 })
 
-local Button = MainTab:CreateButton({
+local FischTab = Window:CreateTab("Fisch", 4483362458) -- Title, Image
+local FischSection = FischTab:CreateSection("Fisch")
+
+local Button = FischTab:CreateButton({
+   Name = "Fisch",
+   Callback = function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/j1w0o/GBS-HUB/refs/heads/main/GBS%20Fisch"))()
+   end,
+})
+
+local FunTab = Window:CreateTab("Universal", 4483362458)
+local FunSection = FunTab:CreateSection("Fling, Fly, TP, etc.")
+
+local Button = FunTab:CreateButton({
+   Name = "Fly",
+   Callback = function()
+       loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\40\39\104\116\116\112\115\58\47\47\103\105\115\116\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\109\101\111\122\111\110\101\89\84\47\98\102\48\51\55\100\102\102\57\102\48\97\55\48\48\49\55\51\48\52\100\100\100\54\55\102\100\99\100\51\55\48\47\114\97\119\47\101\49\52\101\55\52\102\52\50\53\98\48\54\48\100\102\53\50\51\51\52\51\99\102\51\48\98\55\56\55\48\55\52\101\98\51\99\53\100\50\47\97\114\99\101\117\115\37\50\53\50\48\120\37\50\53\50\48\102\108\121\37\50\53\50\48\50\37\50\53\50\48\111\98\102\108\117\99\97\116\111\114\39\41\44\116\114\117\101\41\41\40\41\10\10")()
+   end,
+})
+
+local Button = FunTab:CreateButton({
    Name = "fling",
    Callback = function()
       loadstring(game:HttpGet("https://pastebin.com/raw/e0pP7UEb"))()
    end,
 })
 
-local Button = MainTab:CreateButton({
+local Button = FunTab:CreateButton({
    Name = "punch fling",
    Callback = function()
       loadstring(game:HttpGet("https://pastebin.com/raw/LgZwZ7ZB"))()
    end,
 })
 
-local Tab = Window:CreateTab("Fisch", 4483362458) -- Title, Image
-
-local Button = Tab:CreateButton({
-   Name = "Fisch (💎Premium💎)",
+local Button = FunTab:CreateButton({
+   Name = "Infinity Yield",
    Callback = function()
-      loadstring(game:HttpGet("https://raw.githubusercontent.com/j1w0o/GBS-HUB/refs/heads/main/GBS%20Fisch"))()
+      loadstring(game:HttpGet("https://pastebin.com/raw/prYEhBXK"))()
    end,
 })
